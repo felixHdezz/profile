096e153ec9bdcf1bfeec6b79dfe9474ca12a9095
felixHdez <felix.rescata@gmail.com>
2017-06-08 01:35:38 -0500

    Commit inicial

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Felix Hernandez Hernandez</title>
+        <link rel="icon" type="image/png" href="./image/home.png">
+
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+        <link href="./css/style.css" rel="stylesheet" type="text/css" media="all" />
+    </head>
+
+    <body>
+        <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+            <button class="navbar-toggler navbar-toggler-right btn_toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="e" href="./">
+                <img src="img/índice.png" alt="brand" width="80">
+            </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="./">Inicio <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#skills">Skills</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#education">Educacion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#experien">Experiencia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portafolio">Portafolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#interest">Intereses</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="row profile">
+            <div class="col-sm-3">
+                <div class="dt_profile bdr_solid">
+                    <div class="fond_img"></div>
+                    <div class="info_profile">
+                        <a href="./">
+                            <img class="img_perfil" src="img/perfil.png" />
+                        </a>
+                        <h6 class="nom_user">
+                                <a class="nom" href="./">Félix Hernandez Hernandez</a>
+                            </h6>
+                        <p class="ofic_user">Ingeniero en Tecnologias de la Información, especialidad en sistemas informaticos</p>
+                    </div>
+                </div>
+                <div class="about bdr_solid">
+                    <div class="dt_about">
+                        <h6 class="agd">About <small>·</small></h6>
+                        <ul class="list_profile">
+                            <li><span class="fa fa-suitcase"></span> Worked at <a href="http://www.ybot.studio/">Ybot Studio</a></li>
+                            <li><span class="fa fa-suitcase"></span> Worked at <a href="https://www.rescata.co/">Rescata</a></li>
+                            <li><span class="fa fa-mortar-board"></span>Studied at <a href="http://www.uthh.edu.mx/2015/">UTHH</a></li>
+                            <li><span class="fa fa-home"></span> Lives in <a href="#">Distrito federal, CDMX.</a></li>
+                            <li><span class="fa fa-map-marker"></span> From <a href="#">Chiatipan Huazalingo, Hidalgo.</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-9">
+                <div class="info_experience bdr_solid">
+                    <div class="head_exp">
+                        <h1>Hola! soy <span>Felix Hernandez Hernandez.</span></h1>
+                        <p><span>Software developer & web developer </span></p>
+                        <br>
+                    </div>
+                    <div class="info_profesional info">
+                        <span>¡Hola! Soy Felix Hernandez. Desarrollador Web con poco más de 1 años de experiencia y desarrollo de ChatBots en diferentes plataformas de mensajeria. Experimentado con todas las etapas del ciclo de desarrollo de proyectos web dinámicos. Tener un conocimiento en profundidad, incluyendo HTML5 avanzado, CSS3, JavaScript, jQuery, PHP con base de datos Mysql y MongoDB. Ademas de desarrollo de software de escritorio con c#, visual basic y SQL server.<br>Experiencia en desarrollo de videojuegos didacticos bajo la plataforma de c# con freamwork XNA 4.0.</span>
+                    </div>
+                    <div class="row info-profile info">
+                        <div class="col-sm-6">
+                            <ul class="ul-info">
+                                <li class="li-info">
+                                    <span class="title-info">Edad</span>
+                                    <span class="info">21</span>
+                                </li>
+                                <li class="li-info">
+                                    <span class="title-info">Dirección</span>
+                                    <span class="info">Chiatipan Huazalingo Hidalgo</span>
+                                </li>
+                                <li class="li-info">
+                                    <span class="title-info">Email</span>
+                                    <span class="info">Felix_hdezz@hotmail.com</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6">
+                            <ul class="ul-info">
+                                <li class="li-info">
+                                    <span class="title-info">Telefono</span>
+                                    <span class="info">+52 7712723326</span>
+                                </li>
+                                <li class="li-info">
+                                    <span class="title-info">Sitio web</span>
+                                    <span class="info">www.fHernandez.esy.es</span>
+                                </li>
+                                <li class="li-info">
+                                    <span class="title-info">Nacionalidad</span>
+                                    <span class="info">Mexicana</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="socialLinks">
+                        <h6>Redés sociales</h6>
+                        <ul class="social-links">
+                            <li class="li-social-links bdr_solid">
+                                <a href="https://www.facebook.com/search/top/?q=fhdezz%20call" data-tootik="Facebook" data-tootik-conf="square"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            </li>
+                            <li class="li-social-links bdr_solid">
+                                <a href="https://twitter.com/felix_hdez123" data-tootik="Twitter" data-tootik-conf="square"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            </li>
+                            <li class="li-social-links bdr_solid">
+                                <a href="#" data-tootik="Google Plus" data-tootik-conf="square"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                            </li>
+                            <li class="li-social-links bdr_solid">
+                                <a href="https://www.linkedin.com/in/felix-hernandez-a14041127/" data-tootik="Linkedin" data-tootik-conf="square"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row profile skills" id="skills">
+            <div class="col-sm-12 head_skills head_">
+                <h2><span>SHILLS</span></h2>
+            </div>
+            <div class="col-sm-12">
+                <div class="professional-skills bdr_solid">
+                    <div class="title-skills">
+                        <h5>PROFESSIONAL SKILLS</h5>
+                    </div>
+                    <!-- single skill -->
+                    <div class="skill">
+                        <div class="title-progress">
+                            <span class="skill-name">HTML 5 &amp; CSS 3</span>
+                            <span class="skill-value">90%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress1" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- / single skill -->
+                    <!-- single skill -->
+                    <div class="skill">
+                        <div class="title-progress">
+                            <span class="skill-name">PHP</span>
+                            <span class="skill-value">75%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress2" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- / single skill -->
+                    <!-- single skill -->
+                    <div class="skill">
+                        <div class="title-progress">
+                            <span class="skill-name">JavaScript</span>
+                            <span class="skill-value">85%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress3" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 85%;">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- / single skill -->
+                    <!-- single skill -->
+                    <div class="skill">
+                        <div class="title-progress">
+                            <span class="skill-name">Laravel</span>
+                            <span class="skill-value">70%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress4" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- / single skill -->
+                    <!-- single skill -->
+                    <div class="skill">
+                        <div class="title-progress">
+                            <span class="skill-name">C#</span>
+                            <span class="skill-value">75%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress5" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 85%;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <div class="title-progress">
+                            <span class="skill-name">Python</span>
+                            <span class="skill-value">65%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress5" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 65%;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <div class="title-progress">
+                            <span class="skill-name">Node.js</span>
+                            <span class="skill-value">75%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress5" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="skill">
+                        <div class="title-progress">
+                            <span class="skill-name">SQL server</span>
+                            <span class="skill-value">65%</span>
+                        </div>
+                        <div class="progress">
+                            <div class="progress-bar progress5" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 65%;">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- / single skill -->
+                </div>
+            </div>
+        </div>
+        <div class="row profile educ" id="education">
+            <div class="col-sm-12 head_educt head_">
+                <h2><span>EDUCACION</span></h2>
+            </div>
+            <div class="col-sm-12">
+
+            </div>
+        </div>
+        <div class="row profile exper" id="experien">
+            <div class="col-sm-12 head_exper head_">
+                <h2><span>EXPERIENCIA</span></h2>
+            </div>
+            <div class="col-sm-12">
+                <div class="list_interes">
+                    <ul class="ul_ite_interest">
+                        <li class="item_interest">
+                            <h3><span class="fa fa-home"></span></h3>
+                            <h6>Viajar</h6>
+                        </li>
+                        <li class="item_interest">
+                            <h3><span class="fa fa-home"></span></h3>
+                            <h6>Viajar</h6>
+                        </li>
+                        <li class="item_interest">
+                            <h3><span class="fa fa-home"></span></h3>
+                            <h6>Viajar</h6>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row profile port" id="portafolio">
+            <div class="col-sm-12 head_port head_">
+                <h2><span>POSTAFOLIO</span></h2>
+            </div>
+            <div class="col-sm-12">
+                <div class="list_interes">
+                    <ul class="ul_ite_interest">
+                        <li class="item_interest">
+                            <h3><span class="fa fa-home"></span></h3>
+                            <h6>Viajar</h6>
+                        </li>
+                        <li class="item_interest">
+                            <h3><span class="fa fa-home"></span></h3>
+                            <h6>Viajar</h6>
+                        </li>
+                        <li class="item_interest">
+                            <h3><span class="fa fa-home"></span></h3>
+                            <h6>Viajar</h6>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row profile interest" id="interest">
+            <div class="col-sm-12 head_interest head_">
+                <h2><span>INTERESES</span></h2>
+            </div>
+            <div class="col-sm-12">
+                <div class="list_interes">
+                    <ul class="ul_ite_interest">
+                        <li class="item_interest">
+                            <h3><span class="fa fa-plane"></span></h3>
+                            <h6>Viajar</h6>
+                        </li>
+                        <li class="item_interest">
+                            <h3><span class="fa fa-music"></span></h3>
+                            <h6>Musica</h6>
+                        </li>
+                        <li class="item_interest">
+                            <h3><span class="fa fa-gamepad"></span></h3>
+                            <h6>Jugar</h6>
+                        </li>
+                        <li class="item_interest">
+                            <h3><span class="fa fa-film"></span></h3>
+                            <h6>Peliculas</h6>
+                        </li>
+                        <li class="item_interest">
+                            <h3><span class="fa fa-soccer-ball-o"></span></h3>
+                            <h6>Futbol</h6>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="scroll-top">
+            <span class="fa fa-arrow-up start_html" aria-hidden="true"></span>
+        </div>
+        <!--Init script-->
+        <script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" type="text/javascript" async="async"></script>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" type="text/javascript" async="async"></script>
+        <script>
+            $(document).ready(function () {
+                $('.scroll-top').click(function () {
+                    $("body, html").animate({
+                        scrollTop: '0px'
+                    }, 1050);
+                });
+
+                $(window).scroll(function () {
+                    if ($(this).scrollTop() > 0) {
+                        $('.scroll-top').fadeIn(300);
+                    } else {
+                        $('.scroll-top').slideUp(100);
+                    }
+                });
+                //$('body').scrollspy({target: ".navbar", offset: 50});   
+
+                // Add smooth scrolling on all links inside the navbar
+                $(".nav-item a").on('click', function(event) {
+                    if (this.hash !== "") {
+                        event.preventDefault();
+                        var hash = this.hash;
+                        $('html, body').animate({
+                            scrollTop: $(hash).offset().top
+                        }, 800, function(){
+                            window.location.hash = hash;
+                        });
+                    }
+                });
+            });
+        </script>
+    </body>
+</html>
