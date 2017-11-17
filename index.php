b92e08e44d222fcc3983c2f1f8ef9153114b0137
Félix Hernández Hernández <felix.rescata@gmail.com>
2017-11-17 10:43:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Felix Hernandez Hernandez</title>
+        <title>Felix Hernandez Hernandez |</title>
         <link rel="icon" type="image/png" href="./image/home.png">
 
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
